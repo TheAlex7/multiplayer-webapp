00b65596104c57f2f9546ec1622e4b4fd3fb595c
TheAlex7 <144410177+TheAlex7@users.noreply.github.com>
2025-05-03 16:34:11 -0700

    Trying out phaser framework

---
+++ src/game/scenes/MainMenu.js
@@ -0,0 +1,80 @@
+import { EventBus } from '../EventBus';
+import { Scene } from 'phaser';
+
+export class MainMenu extends Scene
+{
+    logoTween;
+
+    constructor ()
+    {
+        super('MainMenu');
+    }
+
+    create ()
+    {
+        this.add.image(512, 384, 'background');
+
+        this.logo = this.add.image(512, -100, 'logo').setDepth(100);
+
+        this.add.text(512, 460, 'Main Menu', {
+            fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
+            stroke: '#000000', strokeThickness: 8,
+            align: 'center'
+        }).setDepth(100).setOrigin(0.5);
+        
+        EventBus.emit('current-scene-ready', this);
+    }
+
+    changeScene ()
+    {
+        if (this.logoTween)
+        {
+            this.logoTween.stop();
+            this.logoTween = null;
+        }
+
+        this.scene.start('Game');
+    }
+
+    moveLogo (reactCallback)
+    {
+        if (this.logoTween)
+        {
+            if (this.logoTween.isPlaying())
+            {
+                this.logoTween.pause();
+            }
+            else
+            {
+                this.logoTween.play();
+            }
+        }
+        else
+        {
+            this.logoTween = this.tweens.add({
+                targets: this.logo,
+                // x: { value: 0, duration: 1000, ease: 'Back.easeInOut' },
+                y: { value: 300, duration: 1000, ease: 'Bounce.easeOut' },
+                yoyo: true,
+                repeat: 0,
+                onComplete: () => {
+                    // Reset the position to the start of the animation
+                    
+                    this.logoTween.stop();
+                    this.logoTween = null;
+                    
+                    // Restart the tween
+                },
+                onUpdate: () => {
+                    if (reactCallback)
+                    {
+                        reactCallback({
+                            x: Math.floor(this.logo.x),
+                            y: Math.floor(this.logo.y)
+                        });
+                    }
+                }
+            });
+        }
+    }
+}
