993c1c26f3a08e97653450dcb1e29a8183bf8382
Alex <144410177+TheAlex7@users.noreply.github.com>
2025-05-05 01:05:26 -0700

    messing with tweens in the demo

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,10 @@ import { useRef, useState } from 'react';
 import Phaser from 'phaser';
 import { PhaserGame } from './game/PhaserGame';
 
-function App ()
-{
+function App() {
     // The sprite can only be moved in the MainMenu Scene
     const [canMoveSprite, setCanMoveSprite] = useState(true);
-    
+
     //  References to the PhaserGame component (game and scene are exposed)
     const phaserRef = useRef();
     const [spritePosition, setSpritePosition] = useState({ x: 0, y: 0 });
@@ -17,8 +16,7 @@ function App ()
 
         const scene = phaserRef.current.scene;
 
-        if (scene)
-        {
+        if (scene) {
             scene.changeScene();
         }
     }
@@ -27,16 +25,15 @@ function App ()
 
         const scene = phaserRef.current.scene;
 
-        if (scene && scene.scene.key === 'MainMenu')
-        {
+        if (scene && scene.scene.key === 'MainMenu') {
             // Get the update logo position
-            scene.moveLogo(   ({ x, y }) => {
+            scene.moveLogo(({ x, y }) => {
 
                 setSpritePosition({ x, y });
 
             });
 
-            scene.moveStar(   ({ x, y }) => {
+            scene.moveStar(({ x, y }) => {
 
                 setStarPosition({ x, y });
 
@@ -48,24 +45,32 @@ function App ()
 
         const scene = phaserRef.current.scene;
 
-        if (scene)
-        {
+        if (scene) {
             // Add more stars
-            const x = Phaser.Math.Between(64, scene.scale.width - 64);
-            const y = Phaser.Math.Between(64, scene.scale.height - 64);
+            let centerX = Phaser.Math.Between(64, scene.scale.width - 64);
+            let centerY = Phaser.Math.Between(64, scene.scale.height - 64);
+            let center = {centerX, centerY};
 
+            let radius = 20 + Math.random() * 80;
             //  `add.sprite` is a Phaser GameObjectFactory method and it returns a Sprite Game Object instance
-            const star = scene.add.sprite(x, y, 'star');
+            const container = scene.add.container(centerX, centerY);
+            const star = scene.add.sprite(0, 0, 'star');
+            const man = scene.add.sprite(0,0,'man')
+            // container.add(star);
 
             //  ... which you can then act upon. Here we create a Phaser Tween to fade the star sprite in and out.
             //  You could, of course, do this from within the Phaser Scene code, but this is just an example
             //  showing that Phaser objects and systems can be acted upon from outside of Phaser itself.
-            scene.add.tween({
-                targets: star,
-                duration: 500 + Math.random() * 1000,
-                alpha: 0,
-                yoyo: true,
-                repeat: -1
+            scene.tweens.addCounter({
+                duration: 1000 + Math.random() * 2000,
+                from: 0,
+                to: 360 * (Math.random() > .5 ? 1 : -1),
+                repeat: -1,
+                onUpdate: tween => {
+                    const angle = Phaser.Math.DegToRad(tween.getValue());
+                    star.x = centerX + radius * Math.cos(angle);
+                    star.y = centerY + radius * Math.sin(angle);
+                  }
             });
         }
     }
@@ -74,7 +79,7 @@ function App ()
     const currentScene = (scene) => {
 
         setCanMoveSprite(scene.scene.key !== 'MainMenu');
-        
+
     }
 
     return (
